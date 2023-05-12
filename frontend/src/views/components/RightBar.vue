c786895e9ce09ece5ef1033035a4859d8e9cfdd2
Ahsan Hanif <ahsan@timeahead.co>
2023-05-12 10:25:39 +0500

    Added tweet actions for tweets

--- frontend/src/views/components/RightBar.vue
+++ frontend/src/views/components/RightBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col h-full">
-    this is right bar
+    Trends bar goes here
   </div> 
 </template>
 
--- frontend/src/views/components/Tweet.vue
+++ frontend/src/views/components/Tweet.vue
@@ -19,6 +19,23 @@
             <div class="mt-4">
                 <img class="rounded-lg" src="/assets/images/FvFfPANWcAMrSYJ.jpeg" alt="" />
             </div>
+            <div class="flex flex-row items-center justify-between mt-3 text-sm text-theme-gray-dark">
+                <div class="flex flex-row items-center gap-2 cursor-pointer">
+                    <img src="/assets/images/comment.png" alt="" />
+                    <div>12</div>
+                </div>
+                <div class="flex flex-row items-center gap-2 cursor-pointer">
+                    <img src="/assets/images/retweet.png" alt="" />
+                    <div>0</div>
+                </div>
+                <div class="flex flex-row items-center gap-2 cursor-pointer">
+                    <img src="/assets/images/like.png" alt="" />
+                    <div>45</div>
+                </div>
+                <div class="flex flex-row items-center cursor-pointer">
+                    <img src="/assets/images/share.png" alt="" />
+                </div>
+            </div>
         </div>
     </div>
 
